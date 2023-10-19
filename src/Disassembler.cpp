b6275d4be3458f56f6001f511aaf8949348d48b0
André Monteiro <andremgmonteiro01@gmail.com>
2023-10-19 17:44:16 +0100

    Implemented Reading bytes from a file to a Vector

--- src/Disassembler.cpp
+++ src/Disassembler.cpp
@@ -1,6 +1,36 @@
 #include "Disassembler.hpp"
 
+#include <fstream>
+#include <iostream>
+
 void Disassembler::Disassemble(const std::string &file)
 {
+	auto bytes = ReadFile(file);
 
+	for(auto byte : bytes)
+	{
+		std::cout << std::hex << static_cast<int>(byte) << std::endl;
+	}
+}
+
+std::vector<Byte> Disassembler::ReadFile(const std::string &file)
+{
+	auto bytes = std::vector<Byte>();
+
+	std::ifstream input(file, std::ios::binary);
+
+	if(input.fail())
+	{
+		std::cout << "Failed to open file: " << file << std::endl;
+		exit(127);
+	}
+
+	while(!input.eof())
+	{
+		Byte byte;
+		input.read(reinterpret_cast<char*>(&byte), sizeof(Byte));
+		bytes.push_back(byte);
+	}
+
+	return bytes;
 }
--- src/Disassembler.hpp
+++ src/Disassembler.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <cstdint>
+
+using Byte = uint8_t;
 
 class Disassembler
 {
 public:
 	static void Disassemble(const std::string& file);
+
+private:
+	static std::vector<Byte> ReadFile(const std::string& file);
 };
